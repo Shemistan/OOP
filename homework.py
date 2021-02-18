e81f0da3f6702b30c6736bd26e0a7710c929102c
Шамистан <shemistan1@gmail.com>
2021-02-18 22:12:49 +0300

    version2

--- homework.py
+++ homework.py
@@ -4,15 +4,14 @@ DATA_FORMAT = '%d.%m.%Y'
 
 
 class Record:
-    NOW = dt.datetime.now()
 
     def __init__(self, amount, comment, date=None):
         self.amount = amount
         self.comment = comment
-        if type(date) == str:
+        if date is not None:
             date = dt.datetime.strptime(date, DATA_FORMAT)
         else:
-            date = self.NOW
+            date = dt.datetime.now()
         self.date = date.date()
 
 
@@ -20,43 +19,56 @@ class Calculator:
     def __init__(self, limit):
         self.limit = limit
         self.records = []
-        self.today = dt.datetime.now()
-        self.today = self.today.date()
 
     def add_record(self, record):
         self.records.append(record)
 
+    def determine_what_day_it_is(self):
+        self.today = dt.datetime.now()
+        self.today = self.today.date()
+        return self.today
+
     def get_today_stats(self):
-        self.get_today_status = 0
+        self.determine_what_day_it_is()
+        get_today_status = 0
         for recorded_object in self.records:
             if recorded_object.date == self.today:
-                self.get_today_status += recorded_object.amount
-        return self.get_today_status
+                get_today_status += recorded_object.amount
+        return get_today_status
+
+    def weekend(self):
+        self.determine_what_day_it_is()
+        start_day_of_week = self.today
+        self.a_week =[]
+        self.a_week.append(start_day_of_week)
+        for day in range(7):
+            start_day_of_week -= dt.timedelta(days=1)
+            self.a_week.append(start_day_of_week)
 
     def get_week_stats(self):
-        self.start_day_of_week = self.today
-        self.day = 0
-        self.get_week_status = 0
-        while self.day < 7:
-            for recorded_object in self.records:
-                if recorded_object.date == self.start_day_of_week:
-                    self.get_week_status += recorded_object.amount
-            self.start_day_of_week -= dt.timedelta(days=1)
-            self.day += 1
-        return self.get_week_status
+        self.weekend()
+        self.determine_what_day_it_is()
+        get_week_status = 0
+        for recorded_object in self.records:
+            if recorded_object.date in self.a_week:
+                get_week_status += recorded_object.amount
+        return get_week_status
 
-    def calculating_the_remained(self):
+    def calculate_the_remained(self):
         return self.limit - self.get_today_stats()
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
-        if self.limit > self.get_today_stats():
-            return (f'Сегодня можно съесть что-нибудь '
-                    f'ещё, но с общей калорийностью не '
-                    f'более {self.calculating_the_remained()} кКал')
-        else:
-            return 'Хватит есть!'
+        get_today_status = self.get_today_stats()
+        #Самое не понятное замечание было здесь)))
+        #Как же долго я пытался понять, что от меня требуеться
+        #Надеюсь я правильно понял))
+        if self.limit > get_today_status:
+            return ('Сегодня можно съесть что-нибудь '
+                    'ещё, но с общей калорийностью не '
+                    f'более {self.calculate_the_remained()} кКал')
+        return 'Хватит есть!'
 
 
 class CashCalculator(Calculator):
@@ -64,28 +76,25 @@ class CashCalculator(Calculator):
     EURO_RATE = 70.0
 
     def get_today_cash_remained(self, currency):
-        self.currency = currency
-        self.get_today_status = self.get_today_stats()
-
+        get_today_status = self.get_today_stats()
+        if get_today_status == self.limit:
+            return 'Денег нет, держись'
         self.name_currency = {
             'rub': 'руб',
             'usd': 'USD',
             'eur': 'Euro'
         }
-        self.the_remainded = self.calculating_the_remained()
-        self.currency_list = {
-            'rub': self.the_remainded,
-            'usd': round((self.the_remainded / self.USD_RATE), 2),
-            'eur': round((self.the_remainded / self.EURO_RATE), 2)
-        }
+        the_remainded = self.calculate_the_remained()
+        if currency == 'usd':
+            the_remainded = round((the_remainded / self.USD_RATE), 2)
+        elif currency == 'eur':
+            the_remainded = round((the_remainded / self.EURO_RATE), 2)
 
-        if self.get_today_status == self.limit:
-            return 'Денег нет, держись'
-        elif self.get_today_status < self.limit:
+        if self.limit > get_today_status:
             return (f'На сегодня осталось '
-                    f'{self.currency_list[self.currency]} '
-                    f'{self.name_currency[self.currency]}')
-        else:
-            return (f'Денег нет, держись: твой долг - '
-                    f'{abs(self.currency_list[self.currency])} '
-                    f'{self.name_currency[self.currency]}')
+                    f'{the_remainded} '
+                    f'{self.name_currency[currency]}')
+
+        return (f'Денег нет, держись: твой долг - '
+                    f'{abs(the_remainded)} '
+                    f'{self.name_currency[currency]}')
